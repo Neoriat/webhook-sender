0f3439c784b80365cc775853a4f9fbb282df6b60
Neoriat <NeoriatWasNotFound@gmail.com>
2023-06-27 19:08:19 +0430

    -Menu color will be choosen randomly

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import platform
 from os import system
 from time import sleep
 import datetime as dt
+from random import randint
    
 time = dt.datetime.now()
 def clear(): # Clear 
@@ -29,58 +30,85 @@ def send_message(url , content): # Function for sending a message
 
 def chat_as_webhook(): # Function for chat
     clear()
-    url = input(f"[{dt.datetime.now()}] Enter the webhook url:") #get the webhook
+    url = input("Enter the webhook url:") #get the webhook
     if is_webhook_url(url): # if it return true
         while True: # continue until message is equal to q or quit
-            message = input(f"[{dt.datetime.now()}] Enter your message(Enter 'q' to exit):")
+            message = input(color.Fore.RED + "Enter your message(Enter 'q' to exit):")
             match message:
                 case "q":
                     break
                 case _:
+                    print(color.Fore.YELLOW + f"Sending '{message}' ")
                     send_message(url=url , content=message) # sends the messsage to discord
+                    print(color.Fore.GREEN + f"'{message}' has been sent!")
                     
 
         clear()   
         main() # after the loop ends will return you to the main menu
     else:
-        print(f"[{dt.datetime.now()}] Not a valid url")
+        print(f"{url} not a valid url")
         sleep(2)
         chat_as_webhook() # if the function returned false it will do the function again
 
 def spam_webhook():# Spam the webhook
     clear()
-    url = input(f"[{dt.datetime.now()}] Enter the webhook url:") # get the webhook
+    url = input("Enter the webhook url:") # get the webhook
     if is_webhook_url(url):
-        message = input(f"[{dt.datetime.now()}] Enter the message:")
-        count = int(input(f"[{dt.datetime.now()}] How many messages you want to send:"))
-        
+        message = input("Enter the message:")
+        count = int(input("How many messages you want to send:"))
+        count_spam = 1
         for num in range(count):
+            print(color.Fore.YELLOW + f"[{count_spam}] '{message}' has been sent ")
             send_message(url=url , content=message)
+            count_spam += 1
         print(color.Fore.GREEN + "Completed")
         sleep(1)
         clear()
         main()
     else:
+        print(color.Fore.RED + f"{url} is not a valid url")
         clear()  
         spam_webhook()
 
 def send_single_message():
     clear()
-    url = input(f"[{dt.datetime.now()}] Enter the webhook url:") # get the webhook
+    url = input("Enter the webhook url:") # get the webhook
     if is_webhook_url(url): # if it return true
          # continue until message is equal to q 
-        message = input(f"[{dt.datetime.now()}] Enter your message:")
+        message = input("Enter your message:")
+        print(color.Fore.YELLOW + f"Sending '{message}' ")
         send_message(url=url , content=message) # sends the messsage to discord
+        print(color.Fore.GREEN + f"'{message}' has been sent!")
+        sleep(0.50)
         clear()   
         main() # after the loop ends will return you to the main menu
     else:
-        print(f"[{dt.datetime.now()}] Not a valid url")
+        print(f"{url} is not a valid url")
         sleep(2)
         send_single_message()
 
 
 def main():# Main Menu
-    print( color.Fore.RED + """
+
+    main_color_number = randint(1 , 7)
+
+    main_color = None
+
+    match main_color_number:
+        case 1:
+            main_color = color.Fore.GREEN
+        case 2:
+            main_color = color.Fore.BLUE
+        case 3:
+            main_color = color.Fore.RED
+        case 4:
+            main_color = color.Fore.CYAN
+        case 5:
+            main_color = color.Fore.YELLOW
+        case 6:
+            main_color = color.Fore.MAGENTA
+
+    print(main_color + """
 
 __________________________________________                                   
 |                 .                       |
@@ -104,7 +132,7 @@ __________________________________________
 
     """)
     global option
-    option = input( color.Fore.RED + f"[{dt.datetime.now()}] Choose a option:")
+    option = input( color.Fore.RED + "Choose a option:")
     
     clear()
     match option:
@@ -118,7 +146,7 @@ __________________________________________
             send_single_message()
             clear()
         case _:
-            print(f"[{dt.datetime.now()}] Not a valid option")
+            print(f"{option} is not a valid option")
             sleep(2)
             main()
     
